0e18375449efddfeffbe9721381047479babc47a
Vicky Gupta <vickygupta@Vickys-MacBook-Air.local>
2025-08-04 18:01:45 +0530

    Adding filter from api

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,7 +3,7 @@ const RestaurantCard = ({
   name,
   cuisines,
   cloudinaryImageId,
-  lastMileTravelString,
+  sla,
 }) => {
   return (
     <div className="card">
@@ -15,7 +15,7 @@ const RestaurantCard = ({
       />
       <h2>{name}</h2>
       <h3>{cuisines.join(", ")}</h3>
-      <h4>{lastMileTravelString} minutes</h4>
+      <h4>{sla.slaString}</h4>
     </div>
   );
 };
