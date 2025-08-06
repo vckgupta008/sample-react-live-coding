e34908271e599ba39cd43514f8a558918eae671a
Vicky Gupta <vickygupta@Vickys-MacBook-Air.local>
2025-08-06 23:42:38 +0530

    Added tailwind css

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,7 +29,7 @@ const Header = () => {
           <li>
             Online status: {onlineStatus ? "✅" : "❌"}
           </li>
-          <li><Link to={'/'}>Home</Link></li>
+          <li className="text-gray-500"><Link to={'/'}>Home</Link></li>
           <li><Link to={'/about'}>About</Link></li>
           <li><Link to={'/contact'}>Contacts</Link></li>
           <li><Link to={'/grocery'}>Grocery</Link></li>
